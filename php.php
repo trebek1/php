e43bbab31fddf7238dceb6bf4c75bc58969513a0
trebek1 <trebek08@gmail.com>
2015-07-12 18:01:47 -0700

    first php commit

---
+++ php.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+</php echo "Shirts 4 Mike"; ?> 
+<h1>The current server time is <?php echo date("H:i:s a");?></h1>
+<p> &copy;<?php echo date('Y'); ?> Shirts 4 Mike </p>	
+
+</body>
+</html>
+
