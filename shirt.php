bbf310c5de80d37726df45f9620be019d28c6c28
trebek1 <trebek08@gmail.com>
2015-07-14 22:02:42 -0700

    added shirts to individual page

--- shirt.php
+++ shirt.php
@@ -1,1 +1,26 @@
-<?php include("inc/products.php"); ?>
+<?php include("inc/products.php"); 
+
+$product_id = $_GET["id"];
+$product = $products[$product_id];
+
+$section = "shirts"; 
+$pageTitle = $product["name"];
+include("inc/header.php"); ?>
+
+	<div class = "section page">
+
+		<div class = "wrapper"> 
+
+			<div class = "breadcrumb"><a href ='shirts.php'>Shirts</a> &gt; <?php echo $product["name"]; ?> </div>	
+
+			<div class = "shirt-picture">
+				<span>
+					<img src="<?php echo $product["img"]; ?>" alt="<?php echo $product["name"] ?>" >
+				</span>
+			</div>
+		
+		</div>
+
+	</div>
+
+<?php include("inc/footer.php"); ?>
